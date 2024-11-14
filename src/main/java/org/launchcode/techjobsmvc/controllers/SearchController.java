c3503c624a451357cef2a76de9899c51eb4f751a
Charlene Tran <charlenetran@Charlenes-MacBook-Pro.local>
2024-11-14 03:18:26 -0600

    CT completed Task 3 - TestTaskThree passed

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,9 +1,15 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
 
@@ -21,7 +27,31 @@ public class SearchController {
         return "search";
     }
 
+
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam(required = false) String searchTerm) {
+
+        ArrayList<Job> jobs;
+
+        // Uses IF-Else statement to determine where to send the search information
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.isEmpty()) {
+
+            // IF - user enters "all" or leaves searchTerm empty, findAll() method is called and stored in 'jobs' ArrayList
+            jobs = JobData.findAll();
+
+        } else {
+            // ELSE - searchTerm is used in findByColumnAndValue() and stored in 'jobs' ArrayList
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        // Passing jobs & ListController.columnChoices into the View
+        model.addAttribute("jobs",jobs);
+        model.addAttribute("columns",columnChoices);
+
+        // Redirect the user to "/search" (the event listing page)
+        return "redirect:/search";
+    }
 
 }
 
